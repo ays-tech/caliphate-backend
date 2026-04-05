118baee9bacbdca8e522cd5e00e80b675474df41
ays-tech <medrabiu@gmail.com>
2026-04-05 22:17:14 +0100

    new

--- media/prisma/migrations/add_language_format/migration.sql
+++
@@ -1,7 +0,0 @@
--- Create enums
-CREATE TYPE "BookLanguage" AS ENUM ('ENGLISH', 'ARABIC', 'HAUSA', 'FULFULDE');
-CREATE TYPE "BookFormat" AS ENUM ('BOOK', 'AUDIO', 'VIDEO');
-
--- Add columns with defaults so existing rows don't fail
-ALTER TABLE "Book" ADD COLUMN "language" "BookLanguage" NOT NULL DEFAULT 'ARABIC';
-ALTER TABLE "Book" ADD COLUMN "format"   "BookFormat"   NOT NULL DEFAULT 'BOOK';
